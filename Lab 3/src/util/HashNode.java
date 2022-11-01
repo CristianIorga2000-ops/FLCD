0fbbf78683538732a73d73f6c1319050565d49c1
cristian iorga <cristian.iorga@devnest.ro>
2022-11-01 16:47:31 +0200

    Lab 2 done

---
+++ Lab 3/src/util/HashNode.java
@@ -0,0 +1,19 @@
+package util;
+
+// A node of chains
+class HashNode<K, V> {
+    public K key;
+    public V value;
+    public final int hashCode;
+
+    // Reference to next node
+    public HashNode<K, V> next;
+
+    // Constructor
+    public HashNode(K key, V value, int hashCode)
+    {
+        this.key = key;
+        this.value = value;
+        this.hashCode = hashCode;
+    }
+}
